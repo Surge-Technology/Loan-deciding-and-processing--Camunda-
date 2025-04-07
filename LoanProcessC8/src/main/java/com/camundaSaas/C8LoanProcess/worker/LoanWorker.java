d9393efa796770d2cc282c263a20af0e67ea3e61
Balamanchari1248 <111047318+Balamanchari1248@users.noreply.github.com>
2025-04-07 11:40:52 +0530

    bala(07-04)1

--- LoanProcessC8/src/main/java/com/camundaSaas/C8LoanProcess/worker/LoanWorker.java
+++ LoanProcessC8/src/main/java/com/camundaSaas/C8LoanProcess/worker/LoanWorker.java
@@ -34,18 +34,17 @@ public class LoanWorker {
 
 	@Autowired
 	ZeebeClient zeebeClient;
-	
-	RestTemplate restTemplate= new RestTemplate();
-	
+
+	RestTemplate restTemplate = new RestTemplate();
+
 	@Autowired
 	EmailService emailService;
-	
+
 	@Autowired
 	LoanDetailsRepository loanDetailsRepository;
-	
+
 	@Autowired
 	LoanModificationRepository loanModificationRepository;
-	 
 
 	@ZeebeWorker(name = "Persist Customer Information", type = "collection")
 	public void handlePersistCustomerInformation(final JobClient client, final ActivatedJob job) {
@@ -55,162 +54,154 @@ public class LoanWorker {
 		variables.put("assigneeList", users);
 		client.newCompleteCommand(job.getKey()).variables(variables).send().join();
 	}
-	
-	
+
 	@ZeebeWorker(name = "NotificationForUser", type = "NotificationForUser")
 	public void NotificationForUser(final JobClient client, final ActivatedJob job) {
 
-
 		Map<String, Object> variables = new HashMap<>();
 		variables.get("assigneeList");
-		
+
 		client.newCompleteCommand(job.getKey()).variables(variables).send().join();
 	}
 
 	@ZeebeWorker(name = "ApprovalNotification", type = "ApprovalNotification")
 	public void approvalNotification(final JobClient client, final ActivatedJob job) {
-	    System.out.println("Approval Notification");
- 
-	    Map<String, Object> variableasmap = job.getVariablesAsMap();
- 
-	        String url1 = "http://localhost:8080/emailSenderApproval";
- 
-	        HttpHeaders headers = new HttpHeaders();
-	        headers.setContentType(MediaType.APPLICATION_JSON);
- 
-	        Map<String, String> requestBody = new HashMap<>();
-	       // requestBody.put("emailId", "balamanchari@gmail.com"); 
-	        HttpEntity<Map<String, String>> request = new HttpEntity<>(requestBody, headers);
- 
-	        ResponseEntity<String> response1 = restTemplate.postForEntity(url1, request, String.class);
- 
-	        System.out.println("Response: " + response1.getBody());
-	
-	    zeebeClient.newCompleteCommand(job.getKey()).variables("").send().join();
+		System.out.println("Approval Notification");
+
+		Map<String, Object> variableasmap = job.getVariablesAsMap();
+
+		String url1 = "http://localhost:8080/emailSenderApproval";
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		Map<String, String> requestBody = new HashMap<>();
+		// requestBody.put("emailId", "balamanchari@gmail.com");
+		HttpEntity<Map<String, String>> request = new HttpEntity<>(requestBody, headers);
+
+		ResponseEntity<String> response1 = restTemplate.postForEntity(url1, request, String.class);
+
+		System.out.println("Response: " + response1.getBody());
+
+		zeebeClient.newCompleteCommand(job.getKey()).variables("").send().join();
 	}
-	
+
 	@ZeebeWorker(name = "Reject Notification", type = "Reject Notification")
 	public void rejectNotification(final JobClient client, final ActivatedJob job) {
 		System.out.println("Reject Notification");
- 
-	    Map<String, Object> variableasmap = job.getVariablesAsMap();
- 
-	        String url1 = "http://localhost:8080/emailSenderRejection";
- 
-	        HttpHeaders headers = new HttpHeaders();
-	        headers.setContentType(MediaType.APPLICATION_JSON);
- 
-	        Map<String, String> requestBody = new HashMap<>();
-	     //   requestBody.put("emailId", "balamanchari@example.com");
- 
-	        HttpEntity<Map<String, String>> request = new HttpEntity<>(requestBody, headers);
- 
-	        ResponseEntity<String> response1 = restTemplate.postForEntity(url1, request, String.class);
- 
-	        System.out.println("Response: " + response1.getBody());
-	
-	    zeebeClient.newCompleteCommand(job.getKey()).variables("").send().join();
-	}
- 
-	@ZeebeWorker(name = "Clarification Notification", type = "Clarification Notification")
-	public void shipParcel(final JobClient client, final ActivatedJob job) {
-	    Map<String, Object> variableasmap = job.getVariablesAsMap();
- System.out.println("clarification worker");
-        String url1 = "http://localhost:8080/emailSenderClarification";
- 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
- 
-        Map<String, String> requestBody = new HashMap<>();
-     //   requestBody.put("emailId", "balamanchari@example.com");
- 
-        HttpEntity<Map<String, String>> request = new HttpEntity<>(requestBody, headers);
- 
-        ResponseEntity<String> response1 = restTemplate.postForEntity(url1, request, String.class);
- 
-        System.out.println("Response: " + response1.getBody());
- 
+
+		Map<String, Object> variableasmap = job.getVariablesAsMap();
+
+		String url1 = "http://localhost:8080/emailSenderRejection";
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		Map<String, String> requestBody = new HashMap<>();
+		// requestBody.put("emailId", "balamanchari@example.com");
+
+		HttpEntity<Map<String, String>> request = new HttpEntity<>(requestBody, headers);
+
+		ResponseEntity<String> response1 = restTemplate.postForEntity(url1, request, String.class);
+
+		System.out.println("Response: " + response1.getBody());
+
 		zeebeClient.newCompleteCommand(job.getKey()).variables("").send().join();
 	}
-	
+
+	@ZeebeWorker(name = "Clarification Notification", type = "Clarification Notification")
+	public void shipParcel(final JobClient client, final ActivatedJob job) {
+		Map<String, Object> variableasmap = job.getVariablesAsMap();
+		System.out.println("clarification worker");
+		String url1 = "http://localhost:8080/emailSenderClarification";
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		Map<String, String> requestBody = new HashMap<>();
+		// requestBody.put("emailId", "balamanchari@example.com");
+
+		HttpEntity<Map<String, String>> request = new HttpEntity<>(requestBody, headers);
+
+		ResponseEntity<String> response1 = restTemplate.postForEntity(url1, request, String.class);
+
+		System.out.println("Response: " + response1.getBody());
+
+		zeebeClient.newCompleteCommand(job.getKey()).variables("").send().join();
+	}
+
 	@ZeebeWorker(name = "loan Status Update", type = "loan Status Update")
 	public void LoanStatusUpdate(final JobClient client, final ActivatedJob job) {
-	    Map<String, Object> variableasmap = job.getVariablesAsMap();
-	    zeebeClient.newCompleteCommand(job.getKey()).variables("").send().join();
+		Map<String, Object> variableasmap = job.getVariablesAsMap();
+		zeebeClient.newCompleteCommand(job.getKey()).variables("").send().join();
 	}
+
 	@ZeebeWorker(name = "Loan Status Update", type = "Loan Status Update")
 	public void LoanStatusUpdateDisbursement(final JobClient client, final ActivatedJob job) {
-	    Map<String, Object> variableasmap = job.getVariablesAsMap();
-	    zeebeClient.newCompleteCommand(job.getKey()).variables("").send().join();
+		Map<String, Object> variableasmap = job.getVariablesAsMap();
+		zeebeClient.newCompleteCommand(job.getKey()).variables("").send().join();
 	}
-	
-	
-	
+
 	@ZeebeWorker(name = "LoanTermApprovalNotification", type = "LoanTermApprovalNotification")
 	public void LoanTermApprovalNotification(final JobClient client, final ActivatedJob job) {
-		   try {
-		        Map<String, Object> variables = job.getVariablesAsMap();
-		        ObjectMapper objectMapper = new ObjectMapper();
+		try {
+			Map<String, Object> variables = job.getVariablesAsMap();
+			ObjectMapper objectMapper = new ObjectMapper();
 
-		        JsonNode newDataNode = objectMapper.convertValue(variables.get("NewData"), JsonNode.class);
-		        String loanAccountNumber = newDataNode.path("loanAccountNumber").asText();
+			JsonNode newDataNode = objectMapper.convertValue(variables.get("NewData"), JsonNode.class);
+			String loanAccountNumber = newDataNode.path("loanAccountNumber").asText();
 
-		        Optional<Loan> optionalLoan = loanDetailsRepository.findByLoanAccountNumber(loanAccountNumber);
+			Optional<Loan> optionalLoan = loanDetailsRepository.findByLoanAccountNumber(loanAccountNumber);
 
-		        if (optionalLoan.isPresent()) {
-		            Loan loan = optionalLoan.get();
+			if (optionalLoan.isPresent()) {
+				Loan loan = optionalLoan.get();
 
-		            loan.setLoanAmount(newDataNode.path("loanAmount").asText());
-		            loan.setTenure(newDataNode.path("tenure").asInt());
-		            loan.setInterest(newDataNode.path("interestRate").asDouble());
-		            loan.setLoanStatus("Approved"); 
-		            loan.setBillDate(LocalDate.now()); 
+				loan.setLoanAmount(newDataNode.path("loanAmount").asText());
+				loan.setTenure(newDataNode.path("tenure").asInt());
+				loan.setInterest(newDataNode.path("interestRate").asDouble());
+				loan.setLoanStatus("Approved");
+				loan.setBillDate(LocalDate.now());
 
-		            loanDetailsRepository.save(loan);
+				loanDetailsRepository.save(loan);
 
-		            String updatedPayloadJson = objectMapper.writeValueAsString(variables);
-		            LoanModification modification = new LoanModification();
-		            modification.setLoanAccountNumber(loanAccountNumber);
-		            modification.setPayloadJson(updatedPayloadJson);
-		            loanModificationRepository.save(modification);
+				String updatedPayloadJson = objectMapper.writeValueAsString(variables);
+				LoanModification modification = new LoanModification();
+				modification.setLoanAccountNumber(loanAccountNumber);
+				modification.setPayloadJson(updatedPayloadJson);
+				loanModificationRepository.save(modification);
 
-		            String to = "shaukatmakandar786@gmail.com";
-		            String subject = "Loan Term Approval Notification";
-		            String body = "Loan updated successfully for account: " + loanAccountNumber;
-		            emailService.sendSimpleEmail(to, subject, body);
+				String to = "shaukatmakandar786@gmail.com";
+				String subject = "Loan Term Approval Notification";
+				String body = "Loan updated successfully for account: " + loanAccountNumber;
+				emailService.sendSimpleEmail(to, subject, body);
 
-		            client.newCompleteCommand(job.getKey()).send().join();
+				client.newCompleteCommand(job.getKey()).send().join();
 
-		        } else {
-		            throw new RuntimeException("Loan not found with loanAccountNumber: " + loanAccountNumber);
-		        }
+			} else {
+				throw new RuntimeException("Loan not found with loanAccountNumber: " + loanAccountNumber);
+			}
 
-		    } catch (Exception e) {
-		        e.printStackTrace();
-		        client.newFailCommand(job.getKey())
-		              .retries(job.getRetries() - 1)
-		              .errorMessage("Error updating loan: " + e.getMessage())
-		              .send()
-		              .join();
-		    }
+		} catch (Exception e) {
+			e.printStackTrace();
+			client.newFailCommand(job.getKey()).retries(job.getRetries() - 1)
+					.errorMessage("Error updating loan: " + e.getMessage()).send().join();
+		}
 	}
-	
-	
-	
+
 	@ZeebeWorker(name = "Persist Loan Details", type = "Persist Loan Details")
 	public void calculateTenure(final JobClient client, final ActivatedJob job) {
-	        try {
-	            Map<String, Object> variables = job.getVariablesAsMap();
-	          
-	            String apiUrl = "http://localhost:8080/calculateTenureInterestSaveData";
-	            HttpHeaders headers = new HttpHeaders();
-	            headers.setContentType(MediaType.APPLICATION_JSON);
-	            HttpEntity<String> entity = new HttpEntity<>(headers);
-	            ResponseEntity<Map> response = restTemplate.postForEntity(apiUrl, entity, Map.class);
-	            Map<String, Object> responseBody = response.getBody();
-				System.out.println("Loan has been saved...!");
-				String loanAccountNumber = responseBody.get("loanAccountNumber").toString();
-				
+		try {
+			Map<String, Object> variables = job.getVariablesAsMap();
+
+			String apiUrl = "http://localhost:8080/calculateTenureInterestSaveData";
+			HttpHeaders headers = new HttpHeaders();
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			HttpEntity<String> entity = new HttpEntity<>(headers);
+			ResponseEntity<Map> response = restTemplate.postForEntity(apiUrl, entity, Map.class);
+			Map<String, Object> responseBody = response.getBody();
+			System.out.println("Loan has been saved...!");
+			String loanAccountNumber = responseBody.get("loanAccountNumber").toString();
+
 //				response.put("loanId", savedLoan.getLoanId());
 //				response.put("loanAmount", savedLoan.getLoanAmount());
 //				response.put("tenure", savedLoan.getTenure());
@@ -219,99 +210,88 @@ public class LoanWorker {
 //				response.put("loanStatus", loanStatus);
 //				response.put("loanAccountNumber", loanAccountNumber);
 //				response.put("billDate", localDate);
-				
-				int month = (int) responseBody.get("tenure");
-				Integer installmentNo = (Integer) responseBody.get("tenure");
-				Double loanAmount = (Double) responseBody.get("loanAmount");
-				Double annualInterestRate = (Double) responseBody.get("interestRate");
 
-				Double monthlyInterestRate = annualInterestRate / (100 * 12);
-				Double installmentAmount;
-				if (monthlyInterestRate > 0) {
-				    installmentAmount = (loanAmount * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, month)) / 
-				                        (Math.pow(1 + monthlyInterestRate, month) - 1);
-				} else {
-				    installmentAmount = loanAmount / month;
-				}
-				Double closingPrincipal = loanAmount;
-				for (int i = 1; i <= installmentNo; i++) {
-				    Double interestComponent = closingPrincipal * monthlyInterestRate;
-				    Double principalComponent = installmentAmount - interestComponent;
-				    closingPrincipal -= principalComponent;
-				}
-				System.out.println("Installment Amount: " + installmentAmount);
-				System.out.println("Final Closing Principal: " + closingPrincipal);
+			int month = (int) responseBody.get("tenure");
+			Integer installmentNo = (Integer) responseBody.get("tenure");
+			Double loanAmount = (Double) responseBody.get("loanAmount");
+			Double annualInterestRate = (Double) responseBody.get("interestRate");
 
+			Double monthlyInterestRate = annualInterestRate / (100 * 12);
+			Double installmentAmount;
+			if (monthlyInterestRate > 0) {
+				installmentAmount = (loanAmount * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, month))
+						/ (Math.pow(1 + monthlyInterestRate, month) - 1);
+			} else {
+				installmentAmount = loanAmount / month;
+			}
+			Double closingPrincipal = loanAmount;
+			for (int i = 1; i <= installmentNo; i++) {
+				Double interestComponent = closingPrincipal * monthlyInterestRate;
+				Double principalComponent = installmentAmount - interestComponent;
+				closingPrincipal -= principalComponent;
+			}
+			System.out.println("Installment Amount: " + installmentAmount);
+			System.out.println("Final Closing Principal: " + closingPrincipal);
 
-				for (int i = 0; i < 6; i++) {
-					RepaymentSchedule repaymentSchedule = new RepaymentSchedule();
+			for (int i = 0; i < 6; i++) {
+				RepaymentSchedule repaymentSchedule = new RepaymentSchedule();
 
-					LocalDate date = LocalDate.of(2024, month, 11);
-					repaymentSchedule.setInstallmentNo(installmentNo);
-					repaymentSchedule.setInstallmentDate(date);
-					repaymentSchedule.setInstallmentAmount(installmentAmount);
+				LocalDate date = LocalDate.of(2024, month, 11);
+				repaymentSchedule.setInstallmentNo(installmentNo);
+				repaymentSchedule.setInstallmentDate(date);
+				repaymentSchedule.setInstallmentAmount(installmentAmount);
 
-					Double interest = Math.round(closingPrincipal * monthlyInterestRate * 100.0) / 100.0;
-					repaymentSchedule.setInterest(interest);
+				Double interest = Math.round(closingPrincipal * monthlyInterestRate * 100.0) / 100.0;
+				repaymentSchedule.setInterest(interest);
 
-					Double principal = installmentAmount - interest;
-					principal = Math.round(principal * 100.0) / 100.0;
+				Double principal = installmentAmount - interest;
+				principal = Math.round(principal * 100.0) / 100.0;
 
-					if (principal > closingPrincipal) {
-						principal = closingPrincipal;
-						interest = installmentAmount - principal; 
-					}
-
-					repaymentSchedule.setPrincipal(principal);
-
-					closingPrincipal -= principal;
-					closingPrincipal = Math.round(closingPrincipal * 100.0) / 100.0;
-
-					repaymentSchedule.setClosingPrincipal(closingPrincipal);
-
-					repaymentSchedule.setLoanAccountNumber(loanAccountNumber);
-
-					System.out.println("RepaymentSchedule : " + repaymentSchedule);
-
-					String apiUrl1 = "http://localhost:8080/repaymentSchedule/save";
-					HttpHeaders headers1 = new HttpHeaders();
-					headers1.setContentType(MediaType.APPLICATION_JSON);
-					HttpEntity<RepaymentSchedule> entity1 = new HttpEntity<>(repaymentSchedule, headers1);
-
-					ResponseEntity<Map> mapResponseEntity = restTemplate.postForEntity(apiUrl1, entity1, Map.class);
-					Map body = mapResponseEntity.getBody();
-
-					// Increment for Next Installment
-					month += 1;
-					installmentNo += 1;
-
-					if (body != null) {
-						System.out.println("RepaymentSchedule has been saved");
-					}
+				if (principal > closingPrincipal) {
+					principal = closingPrincipal;
+					interest = installmentAmount - principal;
 				}
 
+				repaymentSchedule.setPrincipal(principal);
 
-				if (responseBody != null) {
-	                client.newCompleteCommand(job.getKey())
-	                        .variables(responseBody)
-	                        .send()
-	                        .join();
-	            } else {
-	                client.newCompleteCommand(job.getKey())
-	                        .variables(Map.of("error", "No response from API"))
-	                        .send()
-	                        .join();
-	            }
- 
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	            client.newFailCommand(job.getKey())
-	                    .retries(job.getRetries() - 1)
-	                    .errorMessage("Error in Loan Status Update Worker: " + e.getMessage())
-	                    .send()
-	                    .join();
-	        }
-	    }
-	
+				closingPrincipal -= principal;
+				closingPrincipal = Math.round(closingPrincipal * 100.0) / 100.0;
+
+				repaymentSchedule.setClosingPrincipal(closingPrincipal);
+
+				repaymentSchedule.setLoanAccountNumber(loanAccountNumber);
+
+				System.out.println("RepaymentSchedule : " + repaymentSchedule);
+
+				String apiUrl1 = "http://localhost:8080/repaymentSchedule/save";
+				HttpHeaders headers1 = new HttpHeaders();
+				headers1.setContentType(MediaType.APPLICATION_JSON);
+				HttpEntity<RepaymentSchedule> entity1 = new HttpEntity<>(repaymentSchedule, headers1);
+
+				ResponseEntity<Map> mapResponseEntity = restTemplate.postForEntity(apiUrl1, entity1, Map.class);
+				Map body = mapResponseEntity.getBody();
+
+				// Increment for Next Installment
+				month += 1;
+				installmentNo += 1;
+
+				if (body != null) {
+					System.out.println("RepaymentSchedule has been saved");
+				}
+			}
+
+			if (responseBody != null) {
+				client.newCompleteCommand(job.getKey()).variables(responseBody).send().join();
+			} else {
+				client.newCompleteCommand(job.getKey()).variables(Map.of("error", "No response from API")).send()
+						.join();
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			client.newFailCommand(job.getKey()).retries(job.getRetries() - 1)
+					.errorMessage("Error in Loan Status Update Worker: " + e.getMessage()).send().join();
+		}
+	}
 
 }
